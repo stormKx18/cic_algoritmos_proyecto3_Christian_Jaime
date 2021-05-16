580377d533804fd1c0c023dc0dd15ca5207c095f
stormKx18 <39018761+stormKx18@users.noreply.github.com>
2021-05-16 17:51:56 -0500

    Add files via upload

---
+++ src/Arista.py
@@ -0,0 +1,15 @@
+from random import randint
+#******************************************************************************
+#Clase Arista
+class Arista:
+  def __init__(self, source, target):
+    self.source = source
+    self.target = target
+    self.id = str(source)+' -> '+str(target)
+
+    self.weight = randint(0,50)
+
+    #print(self.id, ' w:', self.weight)
+  def __str__(self):
+    return str(self.id)
+#******************************************************************************
